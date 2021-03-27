ca9def0a218bf42aa09b94f962a141552e176807
CS20BTECH11062 <CS20BTECH11062@iith.ac.in>
2021-03-27 18:10:47 +0530

    changed

--- Assignment-3/codes/Assignment-3.py
+++ Assignment-3/codes/Assignment-3.py
@@ -1,1 +1,19 @@
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+plantA = np.random.exponential(6,250000)
+plantB = np.random.exponential(2,1000000)
+
+works_in_a = 0
+works_in_b = 0
+for i in plantA:
+    if i>12:
+        works_in_a += 1
+for i in plantB:
+    if i>12:
+        works_in_b += 1
+total_works = works_in_b + works_in_a
+prob = works_in_a/total_works
+print(prob)
+
