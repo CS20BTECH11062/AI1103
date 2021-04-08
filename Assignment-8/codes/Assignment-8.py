c5cbb207204fefd3db073da628b956fdc6c39770
Srivatsan T <CS20BTECH11062@iith.ac.in>
2021-04-08 22:36:35 +0530

    added comments

--- Assignment-8/codes/Assignment-8.py
+++ Assignment-8/codes/Assignment-8.py
@@ -1,44 +1,60 @@
+#importing the required packages for plotting, pi symbol and integration
 import numpy as np
 from scipy.integrate import quad
 import random
+import matplotlib.pyplot as plt
 
+#the following defines the given probability density
 def prob_density(t):
     return 1/(np.pi * (1+t**2))
 
+#f_1 and f_2 functions give the probability density of X/3 and Y/3
 def f_1(x):
     return 3 * prob_density(3*x)
 
 def f_2(y):
     return 3 * prob_density(3*y)
 
+#the following function calculates the integrand for convolution function
 def integrand(y,z):
     return f_1(z-y)*f_2(y)
 
+#the quad function performs the integral and gives the convolution function 
 def convolution(z):
     return quad(integrand,-np.inf,np.inf,args=(z))[0]
 
+#the answer function calculates the answer from calculations to check whether it matches with concolution's output
 def ans(z):
     return 6/(np.pi * (4+9*(z**2)))
 
+#We now plot both convolution and ans functions to check it they're the same.
 
-for i in range(10):
-    x = random.randint(1,100)
-    print("[",x,":",convolution(x),":",ans(x),"]")
-
-print("Thus we've verified that our solution is correct")
-
-import matplotlib.pyplot as plt
 x_axis = np.arange(-20,20)
 y_axis_conv = []
 y_axis_init = []
+y_axis_ans = []
 for i in x_axis:
     y_axis_conv.append(convolution(i))
     y_axis_init.append(prob_density(i))
-    
-plt.plot(x_axis,y_axis_conv,color = 'r',label = 'X and Y')
-plt.plot(x_axis,y_axis_init,color = 'b',label = "X+Y/3")
+    y_axis_ans.append(ans(i))
+
+#the following codes plot the initial X,Y pdf's and final X+Y/3 pdf
+plt.subplot(3,1,1)
+plt.plot(x_axis,y_axis_init,color = 'r',label = 'X and Y')
+plt.plot(x_axis,y_axis_conv,color = 'b',label = "X+Y/3")
 plt.xlabel("t")
 plt.ylabel("Probability (variable = t)")
 plt.legend()
+
+#the following code plots the integrated function
+plt.subplot(3,1,2)
+plt.plot(x_axis,y_axis_conv,color = 'b',label = "Integrated function")
+plt.legend()
+
+#the following code plots the ans function to verify answer
+plt.subplot(3,1,3)
+plt.plot(x_axis,y_axis_ans,color = 'g',label = 'Ans function to verify answer')
+plt.legend()
+
 plt.show()
 
